5223be5d8a6606d76074aa30a3a41e6843489878
Mifistoff <michael.tarasyuk91@gmail.com>
2017-06-11 20:02:18 +0300

    Replacing attr_reader for class Car with attr accessor. Changed 'unless' to 'if' in method 'push_break'. Code sructure aligned according to 'ruby style guide'.

--- lib/topics/state_and_behaviour.rb
+++ lib/topics/state_and_behaviour.rb
@@ -4,23 +4,23 @@
 module StateAndBehaviour
   # defines class Car
   class Car
-    attr_reader :color, :model, :year, :current_speed
-    # initialize for Car object
-    def initialize(car_praremeters)
-      @year = car_praremeters[:year] || 2015
-      @color = car_praremeters[:color] || 'purple'
-      @model = car_praremeters[:model] || 'skyline'
-      @current_speed = 0
-    end
-
+    attr_accessor :color, :model, :year, :current_speed
     # create new Car object with default parameters
     def self.default_car
       new
     end
 
+    # initialize for Car object
+    def initialize(car_parameters)
+      @year = car_parameters[:year] || 2015
+      @color = car_parameters[:color] || 'purple'
+      @model = car_parameters[:model] || 'skyline'
+      @current_speed = 0
+    end
+
     # returns decremented current speed if this possible
     def push_break(speed_down_value)
-      return unless @current_speed >= speed_down_value
+      return if @current_speed < speed_down_value
       @current_speed -= speed_down_value
     end
 
