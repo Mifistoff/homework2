8456f650fb7d634aa772e6dc202167397f79d6f6
Mifistoff <michael.tarasyuk91@gmail.com>
2017-06-11 15:16:58 +0300

    Homework #2 Variant #11

--- lib/topics/object_model.rb
+++ lib/topics/object_model.rb
@@ -2,4 +2,27 @@
 
 # exercise object model
 module ObjectModel
+  # defined module Linux Friendly
+  module LinuxFriendly
+    # superclass method fork process
+    def fork_process
+      super
+    end
+  end
+  # parent class for Laptop
+  class PersonalComputer
+    # defined method fork process
+    def fork_process
+      'Parent: allocate memory'
+    end
+
+    # defined method mine bitconins
+    def mine_bitcoins
+      inspect
+    end
+  end
+  # defined new class Laptop with superclass PersonalComputer
+  class Laptop < PersonalComputer
+    prepend LinuxFriendly
+  end
 end
--- lib/topics/state_and_behaviour.rb
+++ lib/topics/state_and_behaviour.rb
@@ -2,4 +2,31 @@
 
 # exercise state and behaviour
 module StateAndBehaviour
+  # defines class Car
+  class Car
+    attr_reader :color, :model, :year, :current_speed
+    # initialize for Car object
+    def initialize(car_praremeters)
+      @year = car_praremeters[:year] || 2015
+      @color = car_praremeters[:color] || 'purple'
+      @model = car_praremeters[:model] || 'skyline'
+      @current_speed = 0
+    end
+
+    # create new Car object with default parameters
+    def self.default_car
+      new
+    end
+
+    # returns decremented current speed if this possible
+    def push_break(speed_down_value)
+      return unless @current_speed >= speed_down_value
+      @current_speed -= speed_down_value
+    end
+
+    # returns incremented current speed
+    def speed_up(speed_up_value)
+      @current_speed += speed_up_value
+    end
+  end
 end
